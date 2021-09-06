b3301d131270b6f3c1c574114ac4ff368a5d48c1
Aravinda <aravinda.sholla@fullthrottlelabs.com>
2021-09-06 20:38:32 +0530

    Add trasaction util

--- app4/models.py
+++ app4/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 
 
 class Blog(models.Model):
@@ -16,3 +16,30 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.headline
+
+    def some(self):
+        cls = self.__class__
+        with transaction.atomic():
+            instance = cls.objects.select_for_update().get(pk=self.pk)
+            instance.status = self.PARSING_STATUS.FAILED
+            instance.reason = "Unable to fetch the resume"
+            instance.save(update_fields=["status", "updated_at", "reason"])
+            return None
+
+    def transact(self, *fields, **field_dict):
+        cls = self.__class__
+        with transaction.atomic():
+            # Locking the object for transaction
+            instance = cls.objects.select_for_update().get(pk=self.pk)
+
+            # fields to be updated
+            update_fields = fields
+
+            # Set values for fields
+            for key, value in field_dict.items():
+                setattr(instance, key, value)
+                update_fields.append(key)
+                # Triggers signal
+                instance.save(update_fields=update_fields)
+
+        return instance
